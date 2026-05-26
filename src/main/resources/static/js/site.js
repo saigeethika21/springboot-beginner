71a2a3a8033d45f9e964ca0b76168e51a9c06f6f
Geethika <pgeethika2113@gmail.com>
2026-05-26 17:48:35 +0530

    Adding toggle button

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,1 +1,17 @@
 console.log("Hello from js");
+var users = [
+    {"name" : "John", "gender" : "Male", "img" : "img/john.png"},
+    {"name" : "Jane", "gender" : "Female", "img" : "img/jane.png"}
+];
+
+var currentUserIndex = 0;
+
+function toggleCard(){
+    currentUserIndex =(currentUserIndex + 1) % 2;
+    var user = users[currentUserIndex];
+
+    // DOM Manipulation
+    document.getElementById("userImage").src = user.img;
+    document.getElementById("userGender").innerHTML = user.gender;
+    document.getElementById("userName").innerHTML = user.name;
+}
